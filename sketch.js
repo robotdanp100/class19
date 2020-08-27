d1c13d1bea14da460fc4a0b0ea37ae6189e861da
robotdanp100 <69824414+robotdanp100@users.noreply.github.com>
2020-08-27 17:27:34 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,50 +1,45 @@
-var wall,thickness;
-var bullet,speed,weight;
+var car,wall;
+var speed,weight;
 function setup() {
   createCanvas(1600,400);
-  bullet = createSprite(50, 200, 50, 50);
-  wall = createSprite(1200, 200, thickness, height/3);
+  car = createSprite(50, 200, 50, 50);
+  wall = createSprite(1500, 200, 60, height/3);
   wall.shapeColor = color(80,80,80);
-  thickness=random(22,83);
-  speed=random(223,321);
-  weight=random(30,52);
+  speed=random(55,90);
+  weight=random(400,1500);
 }
 
 function draw() {
   background(0,0,0);
-  bullet.velocityX = speed;
-  hasCollided();
+  car.velocityX = speed;
+  istouching();
   drawSprites();
 }
 
-function hasCollided(bullet,wall) {
+function istouching() {
+if(car.x-wall.x < car.width/2+wall.width/2 && wall.x-car.x <wall.width/2+car.width/2){
 
-  bulletRightEdge = lbullet.x + lbullet.width;
-  wallLeftEdge = lwall.x;
-  if (bulletRightEdge>=wallLeftEdge){
-  
-  return true
+car.velocityX = 0;
+var deformation=0.5
 
+  if(deformation>180){
+
+  car.shapeColor = color(255,0,0);
+
+  }
+
+  if(deformation<180 && deformation>100){
+
+  car.shapeColor = color(230,230,0);
+
+  }
+
+  if(deformation<100){
+
+  car.shapeColor = color(0,255,0);
+
+  }
 }
+ 
 
-  return false
-
-  if (hasCollided(bullet,wall)) {
-  bullet.velocityX=0
-  var damage=0.5 * weight * speed * speed/(thickness * thickness * thickness);
-
-  if(damage>10){
-  
-  wall.shapeColor=color(255,0,0);
-
-}
-
-  if(damage<10){
-  
-    wall.shapeColor=color(0,255,0);
-  
-  
-}
-
-}
 }
